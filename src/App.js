03500d3926836791cb36497dfed71b1a663d0f54
Tisha Saha <tishasaha32@gmail.com>
2024-05-22 23:32:32 +0530

    update for mobile device app is hidden

--- src/App.js
+++ src/App.js
@@ -10,14 +10,25 @@ import OrderConfirmed from "./pages/OrderConfirmed";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<Product />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/confirmed" element={<OrderConfirmed />} />
-      </Routes>
+      <div className="routes">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/confirmed" element={<OrderConfirmed />} />
+        </Routes>
+      </div>
+
+      <div className="responsiveMessage">
+        <p className="message">Please check this site on Desktop.</p>
+        <br />
+        <p className="message">
+          This site is not yet optimized for mobile. Sorry for the
+          inconvenience.
+        </p>
+      </div>
     </div>
   );
 }
